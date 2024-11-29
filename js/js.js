5604d853c987a164a507ea83b54c55c878b59f46
khkl2588 <62740635+khkl2588@users.noreply.github.com>
2024-11-29 17:01:49 +0900

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,55 @@
-// script.js
-document.querySelector('.contact-button').addEventListener('click', () => {
-    alert('Contact us feature coming soon!');
+const targets = gsap.utils.toArray("div.elementor-widget-container-text_ani");
+
+targets.forEach(target => {
+let SplitClient = new SplitType(target, {type: "lines, words, chars"});
+let lines = SplitClient.lines;
+let words = SplitClient.words;
+let chars = SplitClient.chars;
+
+gsap.from(chars, {
+yPercent: 100,
+autoAlpha: 0,
+duration: 2,
+ease: "circ.out",
+stagger: {
+amount: 1,
+from : "random"
+
+},
+scrollTrigger: {
+trigger: target,
+start: "top bottom",
+end: "+=400",
+markers: false
+}
+});
+});
+
+
+const targets2 = gsap.utils.toArray("div.elementor-widget-container-text_ani2");
+
+targets2.forEach(target => {
+let SplitClient = new SplitType(target, {type: "lines, words, chars"});
+let lines = SplitClient.lines;
+let words = SplitClient.words;
+let chars = SplitClient.chars;
+
+gsap.from(chars, {
+yPercent: 0,
+autoAlpha: 0,
+duration: 1,
+ease: "circ.out",
+stagger: {
+amount: 1,
+from : "random"
+
+},
+scrollTrigger: {
+trigger: target,
+start: "top bottom",
+end: "+=400",
+markers: false
+}
+});
 });
 
